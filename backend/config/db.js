dc39d54cf109a606fd7dc112222e1afb07c71998
Ritika Ghosh <ritikaghoshriti@gmail.com>
2025-06-20 02:54:54 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('todo_db', 'root', 'Rg@040204', {
+const sequelize = new Sequelize('todo_db', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
 });
